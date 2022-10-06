7afa65256eb1cae94339f6c9d0581c6409b6ddba
Edison-Insuasty <edisonins15@gmail.com>
2022-10-06 09:47:38 -0500

    Actualizar Asignar Asesor

--- trabajos/php/AsignarAsesor.php
+++ trabajos/php/AsignarAsesor.php
@@ -1,28 +1,31 @@
 <?php
 	include("conexion.php");
 	include("validarSesion.php");
-	
-	$consulta  = "SELECT idAsesor
-				  FROM asignaasesor
-				  ORDER BY idAsesor DESC
-				  LIMIT 1";
+
+	$consulta  = "SELECT idAsesor FROM asignaasesor ORDER BY idAsesor DESC LIMIT 1";
 	$consulta  = mysqli_query($conexion, $consulta);
 	$consulta  = mysqli_fetch_array($consulta);
 	$idAsesor = $consulta['idAsesor'];
-	
+
 	++$idAsesor;
 
 	$codigodocente = $_POST["codigodocente"];
 	$idtrabajo     = $_POST["idtrabajo"];
 
-	$consultaId = "SELECT idTrabajo
-				   FROM asignaasesor
-				   WHERE idTrabajo = '$idtrabajo' ";
+	$consultaId = "SELECT idTrabajo FROM asignaasesor WHERE idTrabajo = '$idtrabajo' ";
 	$consultaId = mysqli_query($conexion, $consultaId);
 	$consultaId = mysqli_fetch_array($consultaId);
 
-	
-		if(!$consultaId){
+	if(!$consultaId){
+		$consulta4 = " SELECT CodigoDocente FROM docente WHERE CodigoDocente = '$codigodocente' ";
+		$consulta4 = mysqli_query($conexion, $consulta4);
+		$consulta4 = mysqli_fetch_array($consulta4);
+
+		$consulta5 = " SELECT idTrabajo FROM trabajo WHERE idTrabajo = '$idtrabajo' ";
+		$consulta5 = mysqli_query($conexion, $consulta5);
+		$consulta5 = mysqli_fetch_array($consulta5);
+
+		if($consulta4 && $consulta5){
 			$sql = "INSERT INTO asignaasesor VALUES ('$idAsesor', '$codigodocente', '$idtrabajo')";
 
 			$consulta1 = " SELECT CodigoDocente FROM numeroasesorias WHERE CodigoDocente = '$codigodocente' ";
@@ -30,58 +33,38 @@
 			$consulta1 = mysqli_fetch_array($consulta1);
 
 			if ($consulta1){
-				//modificar
 
 				$consulta3 = " SELECT Cantidad FROM numeroasesorias WHERE CodigoDocente = '$codigodocente' ";
 				$consulta3 = mysqli_query($conexion, $consulta3);
-			$consulta3 = mysqli_fetch_array($consulta3);
-			$can = $consulta3['Cantidad'];
+				$consulta3 = mysqli_fetch_array($consulta3);
+				$can = $consulta3['Cantidad'];
 
-			++$can;
+				++$can;
 
-$sql3 = "UPDATE numeroasesorias SET Cantidad='$can' WHERE CodigoDocente = '$codigodocente' ";
+				$sql3 = "UPDATE numeroasesorias SET Cantidad='$can' WHERE CodigoDocente = '$codigodocente' ";
 
-
-if(mysqli_query($conexion, $sql3) ) {
-			echo '<script language="javascript">alert("numeros de asesorias Modificadas.");window.location.href="../perfiladministrador.php"</script>';
-		}else{
-			echo "Error" . $sql3 . "<br>" . mysqli_error($conexion);
-		}
-
-
-
-
-
+				if(mysqli_query($conexion, $sql3) ) {
+				}else{
+					echo "Error" . $sql3 . "<br>" . mysqli_error($conexion);
+				}
 			}else{
+				$consulta2 = "SELECT idNumero FROM numeroasesorias ORDER BY idNumero DESC LIMIT 1";
+				$consulta2  = mysqli_query($conexion, $consulta2);
+				$consulta2  = mysqli_fetch_array($consulta2);
+				$idNumero = $consulta2['idNumero'];
 
+				++$idNumero;
 
-				//guardar
-				$consulta2 = "SELECT idNumero
-				  FROM numeroasesorias
-				  ORDER BY idNumero DESC
-				  LIMIT 1";
-	$consulta2  = mysqli_query($conexion, $consulta2);
-	$consulta2  = mysqli_fetch_array($consulta2);
-	$idNumero = $consulta2['idNumero'];
-	
-	++$idNumero;
+				$cantidad=0;
+				++$cantidad;
 
-	$cantidad=0;
-	++$cantidad;
+				$sql2 = "INSERT INTO numeroasesorias VALUES ('$idNumero', '$codigodocente', '$cantidad')";
 
-	$sql2 = "INSERT INTO numeroasesorias VALUES ('$idNumero', '$codigodocente', '$cantidad')";
-
-	if( mysqli_query($conexion, $sql2) ){
-				echo '<script language="javascript">alert("numeros de asesorias Modificadas.");window.location.href="../perfiladministrador.php"</script>';
-
-	}else{
-				echo "Error" . $sql2 . "<br>" . mysqli_error($conexion);
-	}
-
-
+				if( mysqli_query($conexion, $sql2) ){
+				}else{
+					echo "Error" . $sql2 . "<br>" . mysqli_error($conexion);
+				}
 			}
-
-
 
 			if(mysqli_query($conexion, $sql) ) {
 				echo '<script language="javascript">alert("Asesor asignado.");window.location.href="../perfiladministrador.php"</script>';
@@ -89,8 +72,10 @@ if(mysqli_query($conexion, $sql3) ) {
 				echo "Error" . $sql . "<br>" . mysqli_error($conexion);
 			}
 		}else{
-			echo '<script language="javascript">alert("El trabajo ya tiene asesor.");window.location.href="../perfiladministrador.php"</script>';
+			echo '<script language="javascript">alert("Error en codigo docente o idtrabajo.");window.location.href="../perfiladministrador.php"</script>';
 		}
-	
+	}else{
+		echo '<script language="javascript">alert("El trabajo ya tiene asesor.");window.location.href="../perfiladministrador.php"</script>';
+	}
 	mysqli_close($conexion);
 ?>
