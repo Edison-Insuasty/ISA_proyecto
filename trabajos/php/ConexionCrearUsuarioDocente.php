480c32c03a6819193f9555051f7f1f8ecdd6290d
CamiloMejia99 <camilomp100@hotmail.com>
2022-10-06 10:28:25 -0500

    actualizacion 3

--- trabajos/php/ConexionCrearUsuarioDocente.php
+++ trabajos/php/ConexionCrearUsuarioDocente.php
@@ -11,47 +11,77 @@
 	$ciudad              = $_POST["ciudad"];
 	$especializacion     = $_POST["especializacion"];
 	$password            = $_POST["contraseña"];
-	
-	$passwordHash = password_hash($password, PASSWORD_BCRYPT);
-	
-	$consultaId = "SELECT CodigoDocente
-				   FROM docente
-				   WHERE CodigoDocente = '$codigodocente' ";
-	$consultaId = mysqli_query($conexion, $consultaId);
-	$consultaId = mysqli_fetch_array($consultaId);
+	$repitepassword      = $_POST["repitecontraseña"];
 
-	$consultaId1 = "SELECT CodigoEstudiante
-				   FROM estudiante
-				   WHERE CodigoEstudiante = '$codigodocente' ";
-	$consultaId1 = mysqli_query($conexion, $consultaId1);
-	$consultaId1 = mysqli_fetch_array($consultaId1);
+	if($password == $repitepassword){
+		$passwordHash = password_hash($password, PASSWORD_BCRYPT);
+		
+		$consultaId = "SELECT CodigoDocente FROM docente WHERE CodigoDocente = '$codigodocente' ";
+		$consultaId = mysqli_query($conexion, $consultaId);
+		$consultaId = mysqli_fetch_array($consultaId);
 
-	$consultaId2 = "SELECT CodigoAdministrador
-				   FROM administrador
-				   WHERE CodigoAdministrador = '$codigodocente' ";
-	$consultaId2 = mysqli_query($conexion, $consultaId2);
-	$consultaId2 = mysqli_fetch_array($consultaId2);
+		$consultaId1 = "SELECT CodigoEstudiante FROM estudiante WHERE CodigoEstudiante = '$codigodocente' ";
+		$consultaId1 = mysqli_query($conexion, $consultaId1);
+		$consultaId1 = mysqli_fetch_array($consultaId1);
 
-	$consultaCedula = "SELECT Cedula
-				   FROM docente
-				   WHERE Cedula = '$cedula' ";
-	$consultaCedula = mysqli_query($conexion, $consultaCedula);
-	$consultaCedula = mysqli_fetch_array($consultaCedula);
+		$consultaId2 = "SELECT CodigoAdministrador FROM administrador WHERE CodigoAdministrador = '$codigodocente' ";
+		$consultaId2 = mysqli_query($conexion, $consultaId2);
+		$consultaId2 = mysqli_fetch_array($consultaId2);
 
-	if(!$consultaCedula){
-		if(!$consultaId && !$consultaId1 && !$consultaId2){
-			$sql = "INSERT INTO persona VALUES ('$cedula', '$nombre', '$apellidos', '$correo', '$telefono', '$direccion', '$ciudad')";
-			$sql1 = "INSERT INTO docente VALUES ('$codigodocente', '$cedula', '$especializacion', '$passwordHash')";
-			if(mysqli_query($conexion, $sql) && mysqli_query($conexion, $sql1)) {
-				echo '<script language="javascript">alert("La cuenta del docente ha sido creada exitosamente.");window.location.href="../perfiladministrador.php"</script>';
+		$consultaCedula = "SELECT Cedula FROM docente WHERE Cedula = '$cedula' ";
+		$consultaCedula = mysqli_query($conexion, $consultaCedula);
+		$consultaCedula = mysqli_fetch_array($consultaCedula);
+
+		if(!$consultaCedula){
+			if(!$consultaId && !$consultaId1 && !$consultaId2){
+				$sql = "INSERT INTO persona VALUES ('$cedula', '$nombre', '$apellidos', '$correo', '$telefono', '$direccion', '$ciudad')";
+				$sql1 = "INSERT INTO docente VALUES ('$codigodocente', '$cedula', '$especializacion', '$passwordHash')";
+				if(mysqli_query($conexion, $sql) && mysqli_query($conexion, $sql1)) {
+					$consulta2 = "SELECT idNumero FROM numeroasesorias ORDER BY idNumero DESC LIMIT 1";
+					$consulta2  = mysqli_query($conexion, $consulta2);
+					$consulta2  = mysqli_fetch_array($consulta2);
+					$idNumero = $consulta2['idNumero'];
+					
+					++$idNumero;
+
+					$cantidad=0;
+
+					$sql2 = "INSERT INTO numeroasesorias VALUES ('$idNumero', '$codigodocente', '$cantidad')";
+
+					if( mysqli_query($conexion, $sql2) ){
+
+					}else{
+						echo "Error" . $sql2 . "<br>" . mysqli_error($conexion);
+					}
+
+					$consulta3 = "SELECT idNumeroJ FROM numerojurados ORDER BY idNumeroJ DESC LIMIT 1";
+					$consulta3  = mysqli_query($conexion, $consulta3);
+					$consulta3  = mysqli_fetch_array($consulta3);
+					$idNumeroJ = $consulta3['idNumeroJ'];
+
+					++$idNumeroJ;
+
+					$cantidadJ=0;
+
+					$sql3 = "INSERT INTO numerojurados VALUES ('$idNumeroJ', '$codigodocente', '$cantidadJ')";
+
+					if( mysqli_query($conexion, $sql3) ){
+					}else{
+						echo "Error" . $sql3 . "<br>" . mysqli_error($conexion);
+					}
+
+					echo '<script language="javascript">alert("La cuenta del docente ha sido creada exitosamente.");window.location.href="../perfiladministrador.php"</script>';
+				}else{
+					echo "Error" . $sql . $sql1 . "<br>" . mysqli_error($conexion);
+				}
 			}else{
-				echo "Error" . $sql . $sql1 . "<br>" . mysqli_error($conexion);
+				echo '<script language="javascript">alert("El Codigo ya existe.");window.location.href="../perfiladministrador.php"</script>';
 			}
 		}else{
-			echo '<script language="javascript">alert("El Codigo ya existe.");window.location.href="../perfiladministrador.php"</script>';
+			echo '<script language="javascript">alert("Ya existe un registro con ese numero de cedula.");window.location.href="../perfiladministrador.php"</script>';
 		}
 	}else{
-		echo '<script language="javascript">alert("Ya existe un registro con ese numero de cedula.");window.location.href="../perfiladministrador.php"</script>';
+		echo '<script language="javascript">alert("Contraseñas no coinciden.");window.location.href="../perfiladministrador.php"</script>';
 	}
 	mysqli_close($conexion);
 ?>
