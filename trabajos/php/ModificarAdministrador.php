8e82c16db4a999a77b78fe682597a2324f18b2e0
Edison-Insuasty <edisonins15@gmail.com>
2022-10-06 09:51:12 -0500

    Modificaciones al administrador

--- trabajos/php/ModificarAdministrador.php
+++ trabajos/php/ModificarAdministrador.php
@@ -19,12 +19,11 @@
 	$consu = mysqli_fetch_array($consu);
 
 	if(password_verify($passwordactual, $consu['Password'])){
-
 		$sql = "UPDATE persona SET Nombres='$nombre', Apellidos='$apellidos', Correo='$correo', Telefono='$telefono', Direccion='$direccion', Ciudad='$ciudad' WHERE Cedula = '$_SESSION[cedula]' ";
 		$sql1 = "UPDATE administrador SET Cargo='$cargo', Password='$passwordHash' WHERE CodigoAdministrador = '$_SESSION[codigoadministrador]' ";
 
 		if(mysqli_query($conexion, $sql) && mysqli_query($conexion, $sql1) ) {
-			echo '<script language="javascript">alert("Datos Modificados.");window.location.href="CerrarSesion.php"</script>';
+			echo '<script language="javascript">alert("Datos Modificados.");window.location.href="../perfiladministrador.php"</script>';
 		}else{
 			echo "Error" . $sql . $sql1 . "<br>" . mysqli_error($conexion);
 		}
